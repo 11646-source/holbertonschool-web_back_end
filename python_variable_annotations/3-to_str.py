7dfaaddac5266facbb41f72b310fd94c5ee75cdf
Ryan364412 <bundhoor50@gmail.com>
2026-04-06 18:47:43 +0400

    cnvrt num to str

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+"""
+This module provides a function to convert
+a floating-point number into its string representation.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Convert a float to its string representation.
+
+    Args:
+        n (float): The input floating-point number.
+
+    Returns:
+        str: The string representation of the float.
+    """
+    return str(n)
