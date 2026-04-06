5a3ca61f0ab24b0a4384c9116806bce469f23711
Ryan364412 <bundhoor50@gmail.com>
2026-04-06 18:04:45 +0400

    float

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import math
+
+def floor(n: float) -> int:
+    """
+    Return the floor of a floating-point number.
+
+    Args:
+        n (float): The input number.
+
+    Returns:
+        int: The largest integer less than or equal to n.
+    """
+    return math.floor(n)
+
