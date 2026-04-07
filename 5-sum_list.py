08dbf4998798a0a06159953096ee58f24653a966
Ryan364412 <bundhoor50@gmail.com>
2026-04-07 10:16:51 +0400

    sum-f

---
+++ 5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Takes a list of floats and returns their sum.
+
+    Args:
+        input_list (List[float]): A list of floating-point numbers.
+
+    Returns:
+        float: The sum of all numbers in the list.
+    """
+    return sum(input_list)
