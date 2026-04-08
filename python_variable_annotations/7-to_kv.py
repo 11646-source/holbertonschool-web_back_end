823a5bd3a816fbd3a57e7519db5b097e970ca96b
Ryan364412 <bundhoor50@gmail.com>
2026-04-08 19:45:13 +0400

    tuple

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 """Module for a tuple"""
-from typing import Tuple
+from typing import Tuple, Union
 
 
-def to_kv(k: str, v: float) -> Tuple[str, float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Return a tuple with the string k and the square of v.
 
     Args:
         k (str): The key string.
-        v (float): The value to square.
+        v (int | float): The value to square.
 
     Returns:
         Tuple[str, float]: A tuple containing k and v squared.
     """
-    return (k, v ** 2)
+    return (k, float(v ** 2))
