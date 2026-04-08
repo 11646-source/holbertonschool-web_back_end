a04da3aa9beff25cfe2d35dbda62831f54c2fb34
Ryan364412 <bundhoor50@gmail.com>
2026-04-08 19:38:21 +0400

    tuple

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -2,10 +2,10 @@
 """Module for a tuple"""
 from typing import Union, Tuple
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Takes a string k and an int or float v,
     returns a tuple 
     """
     return (k, float(v ** 2))
-
