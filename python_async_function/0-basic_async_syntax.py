1be6bd35f958c9ab306abc07e9be3bcee17461de
Ryan364412 <bundhoor50@gmail.com>
2026-04-11 10:11:39 +0400

    async

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -3,9 +3,11 @@
 Module representing a function for a basic async program.
 """
 
+
 import asyncio
 import random
 import importlib.util
+
 
 async def wait_random(max_delay: int = 10) -> float:
     """
