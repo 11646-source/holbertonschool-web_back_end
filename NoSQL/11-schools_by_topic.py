6aedd77d1d43b0ed951df590a249a4d13363f5b8
Ryan364412 <bundhoor50@gmail.com>
2026-04-29 09:50:12 +0400

    SCT

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,14 @@
+#!/bin/usr/env python3
+"""
+Module that return a list function of school having specific topic
+"""
+def schools_by_topic(mongo_collection, topic):
+    """
+    Returns a list of schools having a specific topic.
+
+    :param mongo_collection: pymongo collection object
+    :param topic: string topic to search for
+    :return: list of matching school documents
+    """
+    schools = mongo_collection.find({"topics": topic})
+    return list(schools)
