3e3ef79dfa7ec696d2f97a73588e9f70fe4ca28d
Ryan364412 <bundhoor50@gmail.com>
2026-04-29 11:22:15 +0400

    constant

---
+++ ES6_basic/0-constants.js
@@ -0,0 +1,10 @@
+function taskFirst() {
+  const firstVariable = "I am constant";
+  return firstVariable;
+}
+
+function taskNext() {
+  let nextVariable = "I can change";
+  nextVariable = "I have changed!";
+  return nextVariable;
+}
