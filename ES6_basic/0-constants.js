6fa59bed27c6aa1ad2f2456a16a29699d4b2e41f
Ryan364412 <bundhoor50@gmail.com>
2026-04-29 12:55:57 +0400

    constant

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,10 +1,15 @@
-function taskFirst() {
-  const firstVariable = "I am constant";
-  return firstVariable;
+export function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
 }
 
-function taskNext() {
-  let nextVariable = "I can change";
-  nextVariable = "I have changed!";
-  return nextVariable;
+export function getLast() {
+  return ' is okay';
+}
+
+export function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
 }
