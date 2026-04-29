248b900fc75287d15100e665a62c472acf561798
Ryan364412 <bundhoor50@gmail.com>
2026-04-29 20:55:19 +0400

    block

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,29 +3,9 @@ export default function taskBlock(trueOrFalse) {
   var task2 = true;
 
   if (trueOrFalse) {
-    // use different names so they don't overwrite
-    var innerTask = true;
-    var innerTask2 = false;
-
-    // assign back to the outer variables
-    task = innerTask;
-    task2 = innerTask2;
-  }
-
-  return [task, task2];
-}
-export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
-
-  if (trueOrFalse) {
-    // use different names so they don't overwrite
-    var innerTask = true;
-    var innerTask2 = false;
-
-    // assign back to the outer variables
-    task = innerTask;
-    task2 = innerTask2;
+    // don’t redeclare with var, just reassign
+    task = true;
+    task2 = false;
   }
 
   return [task, task2];
