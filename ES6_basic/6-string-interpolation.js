1e91a7f5aec485eb7cb7dba9427cd9272dce16ff
Ryan364412 <bundhoor50@gmail.com>
2026-05-03 10:52:08 +0400

    S.interpreter

--- ES6_basic/6-string-interpolation.js
+++ ES6_basic/6-string-interpolation.js
@@ -2,5 +2,6 @@ export default function getSanFranciscoDescription() {
 	const name = "San Fransico";
 	const state = "California
 	const population = "884,363";
+	const year = "2020";
 
 	return `${name} is a city in ${state} with a population of ${population}.`;
