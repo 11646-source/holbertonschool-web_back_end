28b9d37de7a634ccbd10a807f746f1d8437391b9
Ryan364412 <bundhoor50@gmail.com>
2026-05-05 19:16:20 +0400

    pricing

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,32 +1,36 @@
 import Currency from './3-currency.js';
 
 export default class Pricing {
-	constructor(amount, currency) {
-		this.amount = amount;
-		this.currency = currency;
-	}
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
 
-	get amount() {
-		return this._amount;
-	}
+  // Getter for amount
+  get amount() {
+    return this._amount;
+  }
 
-	set amout(value) {
-		return this._amount = value;
-	}
+  // Setter for amount
+  set amount(value) {
+    this._amount = value;
+  }
 
-	get currency() {
-		return this._currency
-	}
+  // Getter for currency
+  get currency() {
+    return this._currency;
+  }
 
-	set currency(value) {
-		return this._currency = value;
-	}
+  // Setter for currency
+  set currency(value) {
+    this._currency = value;
+  }
 
-	displayFullPrice() {
-		return `${this._amount} ${this._currency.name} (${this._currency.code})`;
-	}
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
 
-	static covertPrice(amount, conversionRate) {
-		return amount * conversionRate;
-	}
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 }
