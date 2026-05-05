c89c4472775503184cb5da26a1fed7017ed52814
Ryan364412 <bundhoor50@gmail.com>
2026-05-06 00:03:29 +0400

    hosting

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -33,7 +33,7 @@ export class StudentHolberton {
   }
 }
 
-// Create class instances AFTER class definitions
+// Create instances
 const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
@@ -43,4 +43,7 @@ const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-export const listOfStudents = [student1, student2, student3, student4, student5];
+// ✅ Export AFTER initialization
+const listOfStudents = [student1, student2, student3, student4, student5];
+
+export { listOfStudents };
