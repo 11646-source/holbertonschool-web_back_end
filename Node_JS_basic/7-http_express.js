a795d10f3a77e2cc78dd1388c9e750ca8990dfc3
Ryan364412 <bundhoor50@gmail.com>
2026-07-23 18:25:06 +0400

    express

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,32 +1,28 @@
-// 7-http_express.js
-
 const express = require('express');
 const countStudents = require('./3-read_file_async');
 
 const app = express();
-
-const database = process.argv[2];
+const port = 1245;
 
 app.get('/', (req, res) => {
   res.setHeader('Content-Type', 'text/plain');
-  res.status(200).send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
 app.get('/students', async (req, res) => {
   res.setHeader('Content-Type', 'text/plain');
-
-  let response = 'This is the list of our students';
-
+  const databasePath = process.argv[2];
+  res.write('This is the list of our students\n');
   try {
-    const students = await countStudents(database);
-    response += `\n${students}`;
-    res.status(200).send(response);
-  } catch (err) {
-    response += `\n${err.message}`;
-    res.status(500).send(response);
+    const output = await countStudents(databasePath);
+    res.end(output);
+  } catch (error) {
+    res.end(error.message);
   }
 });
 
-app.listen(1245);
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
 
 module.exports = app;
